ec9c463538a5a81cc984054be6e16e4a0ddf50c0
Shalini0001 <64401991+Shalini0001@users.noreply.github.com>
2020-12-05 14:25:19 +0530

    Add files via upload

---
+++ lib/splash_screen.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+
+import 'package:animation1_app/second_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => StartState();
+}
+
+class StartState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return initScreen(context);
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  startTimer() async {
+    var duration = Duration(seconds: 5);
+    return new Timer(duration, route);
+  }
+
+  route() {
+    Navigator.pushReplacement(context, MaterialPageRoute(
+        builder: (context) => MyApp()
+      )
+    );
+  }
+
+  initScreen(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Image.network("https://www.pngitem.com/pimgs/m/186-1866042_best-cars-in-2017-hd-png-download.png"),
+            ),
+            Padding(padding: EdgeInsets.only(top: 20.0)),
+            Text(
+              "Welcome to Animated App",
+              style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 20.0)),
+            CircularProgressIndicator(
+              backgroundColor: Colors.white,
+              strokeWidth: 1,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
